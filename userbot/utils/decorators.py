dc0f534bd9b4e9f42048aa2e7b9edf4c8aeff954
telethontesthelp <87152880+telethontesthelp@users.noreply.github.com>
2021-07-10 01:09:38 +0300

    Update decorators.py

--- userbot/utils/decorators.py
+++ userbot/utils/decorators.py
@@ -10,7 +10,7 @@ from ..Config import Config
 from ..core.data import _sudousers_list, blacklist_chats_list
 from ..core.events import MessageEdited, NewMessage
 from ..core.logger import logging
-from ..core.session import catub
+from ..core.session import iqthon
 from ..helpers.utils.format import paste_message
 from ..helpers.utils.utils import runcmd
 from ..sql_helper.globals import gvarstatus
@@ -219,8 +219,8 @@ def register(**args):
 
     def decorator(func):
         if not disable_edited:
-            catub.add_event_handler(func, MessageEdited(**args))
-        catub.add_event_handler(func, NewMessage(**args))
+            iqthon.add_event_handler(func, MessageEdited(**args))
+        iqthon.add_event_handler(func, NewMessage(**args))
         try:
             LOAD_PLUG[file_test].append(func)
         except Exception:
@@ -276,8 +276,8 @@ def command(**args):
 
     def decorator(func):
         if allow_edited_updates:
-            catub.add_event_handler(func, MessageEdited(**args))
-        catub.add_event_handler(func, NewMessage(**args))
+            iqthon.add_event_handler(func, MessageEdited(**args))
+        iqthon.add_event_handler(func, NewMessage(**args))
         try:
             LOAD_PLUG[file_test].append(func)
         except BaseException:
