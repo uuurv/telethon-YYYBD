2270cdd5d9572451ffb84467da554e961ea8cc8b
telethontesthelp <87152880+telethontesthelp@users.noreply.github.com>
2021-07-10 01:12:59 +0300

    Update pluginmanager.py

--- userbot/utils/pluginmanager.py
+++ userbot/utils/pluginmanager.py
@@ -8,7 +8,7 @@ from ..Config import Config
 from ..core import LOADED_CMDS, PLG_INFO
 from ..core.logger import logging
 from ..core.managers import edit_delete, edit_or_reply
-from ..core.session import catub
+from ..core.session import iqthon
 from ..helpers.tools import media_type
 from ..helpers.utils import _cattools, _catutils, _format, install_pip, reply_id
 from .decorators import admin_cmd, sudo_cmd
@@ -37,11 +37,11 @@ def load_module(shortname, plugin_path=None):
         checkplugins(path)
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
-        mod.bot = catub
+        mod.bot = iqthon
         mod.LOGS = LOGS
         mod.Config = Config
         mod._format = _format
-        mod.tgbot = catub.tgbot
+        mod.tgbot = iqthon.tgbot
         mod.sudo_cmd = sudo_cmd
         mod.CMD_HELP = CMD_HELP
         mod.reply_id = reply_id
@@ -54,7 +54,7 @@ def load_module(shortname, plugin_path=None):
         mod.parse_pre = _format.parse_pre
         mod.edit_or_reply = edit_or_reply
         mod.logger = logging.getLogger(shortname)
-        mod.borg = catub
+        mod.borg = iqthon
         spec.loader.exec_module(mod)
         # for imports
         sys.modules["userbot.plugins." + shortname] = mod
@@ -71,23 +71,23 @@ def remove_plugin(shortname):
         for cmdname in cmd:
             if cmdname in LOADED_CMDS:
                 for i in LOADED_CMDS[cmdname]:
-                    catub.remove_event_handler(i)
+                    iqthon.remove_event_handler(i)
                 del LOADED_CMDS[cmdname]
         return True
     except Exception as e:
         LOGS.error(e)
     try:
         for i in LOAD_PLUG[shortname]:
-            catub.remove_event_handler(i)
+            iqthon.remove_event_handler(i)
         del LOAD_PLUG[shortname]
     except BaseException:
         pass
     try:
         name = f"userbot.plugins.{shortname}"
-        for i in reversed(range(len(catub._event_builders))):
-            ev, cb = catub._event_builders[i]
+        for i in reversed(range(len(iqthon._event_builders))):
+            ev, cb = iqthon._event_builders[i]
             if cb.__module__ == name:
-                del catub._event_builders[i]
+                del iqthon._event_builders[i]
     except BaseException:
         raise ValueError
 
