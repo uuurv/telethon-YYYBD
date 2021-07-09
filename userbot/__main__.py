abead7b5108bc902cc45580676b3fd64911116d3
telethontesthelp <87152880+telethontesthelp@users.noreply.github.com>
2021-07-09 23:17:38 +0300

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -50,8 +50,8 @@ async def startup_process():
     print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
     print("Yay your userbot is officially working.!!!")
     print(
-        f"Congratulation, now type {cmdhr}alive to see message if catub is live\
-        \nIf you need assistance, head to https://t.me/catuserbot_support"
+        f"Congratulation, now type {cmdhr}alive to see message if iqthon is live\
+        \nIf you need assistance, head to https://t.me/iqthon"
     )
     print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
     await verifyLoggerGroup()
