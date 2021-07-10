d134ef9c51e4fbe1eef172abd7619bade35b72d5
telethontesthelp <87152880+telethontesthelp@users.noreply.github.com>
2021-07-10 22:24:52 +0300

    Update calc.py

--- userbot/plugins/calc.py
+++ userbot/plugins/calc.py
@@ -2,12 +2,12 @@ import io
 import sys
 import traceback
 
-from . import catub, edit_or_reply
+from . import iqthon, edit_or_reply
 
 plugin_category = "utils"
 
 
-@catub.cat_cmd(
+@iqthon.iq_cmd(
     pattern="calc ([\s\S]*)",
     command=("calc", plugin_category),
     info={
