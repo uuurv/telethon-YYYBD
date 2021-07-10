4fa0a983a70b8ef2b1712d97c1eede131aedb7d2
telethontesthelp <87152880+telethontesthelp@users.noreply.github.com>
2021-07-10 22:13:24 +0300

    Update blacklistchats.py

--- userbot/plugins/blacklistchats.py
+++ userbot/plugins/blacklistchats.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from telethon.utils import get_display_name
 
-from userbot import catub
+from userbot import iqthon
 from userbot.core.logger import logging
 
 from ..core.data import blacklist_chats_list
@@ -15,7 +15,7 @@ plugin_category = "tools"
 LOGS = logging.getLogger(__name__)
 
 
-@catub.cat_cmd(
+@iqthon.iq_cmd(
     pattern="chatblacklist (on|off)$",
     command=("chatblacklist", plugin_category),
     info={
@@ -71,7 +71,7 @@ async def chat_blacklist(event):
     await edit_delete(event, "It was turned off already")
 
 
-@catub.cat_cmd(
+@iqthon.iq_cmd(
     pattern="addblkchat(s)?(?:\s|$)([\s\S]*)",
     command=("addblkchat", plugin_category),
     info={
@@ -150,7 +150,7 @@ async def add_blacklist_chat(event):
     await event.client.reload(msg)
 
 
-@catub.cat_cmd(
+@iqthon.iq_cmd(
     pattern="rmblkchat(s)?(?:\s|$)([\s\S]*)",
     command=("rmblkchat", plugin_category),
     info={
@@ -214,7 +214,7 @@ async def add_blacklist_chat(event):
     await event.client.reload(msg)
 
 
-@catub.cat_cmd(
+@iqthon.iq_cmd(
     pattern="listblkchats$",
     command=("listblkchats", plugin_category),
     info={
