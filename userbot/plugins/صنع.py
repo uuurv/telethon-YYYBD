99ed0a484426a1d441df0237fd36d52c4778fe05
telethontesthelp <87152880+telethontesthelp@users.noreply.github.com>
2021-07-11 02:47:50 +0300

    Update صنع.py

--- userbot/plugins/صنع.py
+++ userbot/plugins/صنع.py
@@ -5,7 +5,7 @@ from ..Config import Config
 from ..core.managers import edit_delete, edit_or_reply
 from ..utils.tools import create_supergroup
 
-plugin_category = "اوامر الحساب"
+plugin_category = "tools"
 
 
 @iqthon.iq_cmd(
