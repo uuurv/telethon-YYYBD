5a60244bf03a8393f5ad278a8cc1c1a905176fb8
telethontesthelp <87152880+telethontesthelp@users.noreply.github.com>
2021-07-10 23:47:17 +0300

    Update fake.py

--- userbot/plugins/fake.py
+++ userbot/plugins/fake.py
@@ -5,7 +5,7 @@ from telethon.errors import BadRequestError
 from telethon.tl.functions.channels import EditAdminRequest
 from telethon.tl.types import ChatAdminRights
 
-from userbot import catub
+from userbot import iqthon
 
 from ..core.managers import edit_delete, edit_or_reply
 from ..helpers.utils import get_user_from_event
@@ -14,7 +14,7 @@ from . import ALIVE_NAME
 plugin_category = "fun"
 
 
-@catub.cat_cmd(
+@iqthon.iq_cmd(
     pattern="scam(?:\s|$)([\s\S]*)",
     command=("scam", plugin_category),
     info={
@@ -78,7 +78,7 @@ async def _(event):
         return
 
 
-@catub.cat_cmd(
+@iqthon.iq_cmd(
     pattern="prankpromote(?:\s|$)([\s\S]*)",
     command=("prankpromote", plugin_category),
     info={
@@ -110,7 +110,7 @@ async def _(event):
     await catevent.edit("`Promoted Successfully! Now gib Party`")
 
 
-@catub.cat_cmd(
+@iqthon.iq_cmd(
     pattern="padmin$",
     command=("padmin", plugin_category),
     info={
