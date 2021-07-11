94e40f8c7a39dfdfac38e3d7fd186609c0c760d6
telethontesthelp <87152880+telethontesthelp@users.noreply.github.com>
2021-07-12 02:45:34 +0300

    Update scrapers.py

--- userbot/plugins/scrapers.py
+++ userbot/plugins/scrapers.py
@@ -5,7 +5,7 @@ from pySmartDL import SmartDL
 from wikipedia import summary
 from wikipedia.exceptions import DisambiguationError, PageError
 
-from userbot import catub
+from userbot import iqthon
 
 from ..core.managers import edit_or_reply
 from ..helpers.functions import get_cast, get_moviecollections, imdb, mov_titles
@@ -16,7 +16,7 @@ plugin_category = "utils"
 moviepath = os.path.join(os.getcwd(), "temp", "moviethumb.jpg")
 
 
-@catub.cat_cmd(
+@iqthon.iq_cmd(
     pattern="wiki ([\s\S]*)",
     command=("wiki", plugin_category),
     info={
@@ -64,7 +64,7 @@ async def wiki(event):
         )
 
 
-@catub.cat_cmd(
+@iqthon.iq_cmd(
     pattern="imdb ([\s\S]*)",
     command=("imdb", plugin_category),
     info={
