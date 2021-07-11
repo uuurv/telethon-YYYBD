9db05168ff8f8acefcb3b0af84d1045b30d22c68
telethontesthelp <87152880+telethontesthelp@users.noreply.github.com>
2021-07-12 02:45:48 +0300

    Update speedtest.py

--- userbot/plugins/speedtest.py
+++ userbot/plugins/speedtest.py
@@ -6,7 +6,7 @@ from time import time
 
 import speedtest
 
-from userbot import catub
+from userbot import iqthon
 
 from ..core.managers import edit_or_reply
 from ..helpers.utils import reply_id
@@ -24,7 +24,7 @@ def convert_from_bytes(size):
     return f"{round(size, 2)} {units[n]}"
 
 
-@catub.cat_cmd(
+@iqthon.iq_cmd(
     pattern="speedtest(?:\s|$)([\s\S]*)",
     command=("speedtest", plugin_category),
     info={
