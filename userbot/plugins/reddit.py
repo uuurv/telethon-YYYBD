6e56024257893f1e800f185a06e03bca6a953d41
telethontesthelp <87152880+telethontesthelp@users.noreply.github.com>
2021-07-12 02:37:17 +0300

    Update reddit.py

--- userbot/plugins/reddit.py
+++ userbot/plugins/reddit.py
@@ -7,7 +7,7 @@
 
 import requests
 
-from userbot import catub
+from userbot import iqthon
 
 from ..core.logger import logging
 from ..core.managers import edit_delete
@@ -21,7 +21,7 @@ API = "https://meme-api.herokuapp.com/gimme"
 plugin_category = "misc"
 
 
-@catub.cat_cmd(
+@iqthon.iq_cmd(
     pattern="reddit(?:\s|$)([\s\S]*)",
     command=("reddit", plugin_category),
     info={
