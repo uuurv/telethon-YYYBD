74b03ca4d9690d3b6562c6a56ecfe3c92115f3d4
telethontesthelp <87152880+telethontesthelp@users.noreply.github.com>
2021-07-12 04:29:40 +0300

    Update transfer_channel.py

--- userbot/plugins/transfer_channel.py
+++ userbot/plugins/transfer_channel.py
@@ -5,14 +5,14 @@ Available Commands:
 import telethon.password as pwd_mod
 from telethon.tl import functions
 
-from userbot import catub
+from userbot import iqthon
 
 from ..Config import Config
 
 plugin_category = "utils"
 
 
-@catub.cat_cmd(
+@iqthon.iq_cmd(
     pattern="otransfer ([\s\S]*)",
     command=("otransfer", plugin_category),
     info={
