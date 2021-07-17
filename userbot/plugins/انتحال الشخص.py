c2890ab91a303b0a4ec12de7c3730bdf8bbea8c5
telethontesthelp <87152880+telethontesthelp@users.noreply.github.com>
2021-07-17 22:16:24 +0300

    Update and rename clone.py to انتحال الشخص.py

--- userbot/plugins/انتحال الشخص.py
+++ userbot/plugins/انتحال الشخص.py
@@ -1,4 +1,3 @@
-# Credits of Plugin @ViperAdnan and @mrconfused(revert)[will add sql soon]
 import html
 
 from telethon.tl import functions
@@ -21,13 +20,13 @@ DEFAULTUSER = str(AUTONAME) if AUTONAME else str(ALIVE_NAME)
 DEFAULTUSERBIO = (
     str(DEFAULT_BIO)
     if DEFAULT_BIO
-    else "sıɥʇ ǝpoɔǝp uǝɥʇ llıʇu∩ ˙ ǝɔɐds ǝʇɐʌıɹd ǝɯos ǝɯ ǝʌı⅁˙"
+    else "الحمد الله على كل شئ  ⌔︙ @IQTHON"
 )
 
 
 @iqthon.iq_cmd(
-    pattern="clone(?:\s|$)([\s\S]*)",
-    command=("clone", plugin_category),
+    pattern="انتحال(?: |$)(.*)",
+    command=("انتحال", plugin_category),
     info={
         "header": "To clone account of mentiond user or replied user",
         "usage": "{tr}clone <username/userid/reply>",
@@ -58,17 +57,17 @@ async def _(event):
     await event.client(functions.account.UpdateProfileRequest(about=user_bio))
     pfile = await event.client.upload_file(profile_pic)
     await event.client(functions.photos.UploadProfilePhotoRequest(pfile))
-    await edit_delete(event, "**LET US BE AS ONE**")
+    await edit_delete(event, "**تم انتحال الحساب بنجاح ✅**")
     if BOTLOG:
         await event.client.send_message(
             BOTLOG_CHATID,
-            f"#CLONED\nsuccessfully cloned [{first_name}](tg://user?id={user_id })",
+            f"#الأنـتحال\nتم النـسخ بنجـاح  [{first_name}](tg://user?id={user_id })",
         )
 
 
 @iqthon.iq_cmd(
-    pattern="revert$",
-    command=("revert", plugin_category),
+    pattern="اعادة$",
+    command=("اعادة", plugin_category),
     info={
         "header": "To revert back to your original name , bio and profile pic",
         "note": "For proper Functioning of this command you need to set AUTONAME and DEFAULT_BIO with your profile name and bio respectively.",
@@ -88,8 +87,8 @@ async def _(event):
     await event.client(functions.account.UpdateProfileRequest(about=bio))
     await event.client(functions.account.UpdateProfileRequest(first_name=name))
     await event.client(functions.account.UpdateProfileRequest(last_name=blank))
-    await edit_delete(event, "successfully reverted to your account back")
+    await edit_delete(event, "**تم بنجاح اعادة حسابك ✅**")
     if BOTLOG:
         await event.client.send_message(
-            BOTLOG_CHATID, f"#REVERT\nsuccessfully reverted back to your profile"
+            BOTLOG_CHATID, f"#الأعـادة\nتم بـنجاح اعادة حسابك الى وضعه الطبيعي**"
         )
