c781eb768dade7cbe848a20e05d099070efb2d97
telethontesthelp <87152880+telethontesthelp@users.noreply.github.com>
2021-07-20 15:54:39 +0300

    Update and rename alive.py to السورس.py

--- userbot/plugins/السورس.py
+++ userbot/plugins/السورس.py
@@ -24,8 +24,8 @@ plugin_category = "utils"
 
 
 @iqthon.iq_cmd(
-    pattern="alive$",
-    command=("alive", plugin_category),
+    pattern="السورس$",
+    command=("السورس", plugin_category),
     info={
         "header": "To check bot's alive status",
         "options": "To show media in this cmd you need to set ALIVE_PIC with media link, get this by replying the media by .tgm",
