24e43d4b48e98941c6bf2b260b3082c75a9d7b65
i.q <64720410+klanrali@users.noreply.github.com>
2021-08-08 00:52:41 +0300

    Update and rename schedule.py to رساله وقتيه.py

--- userbot/plugins/رساله وقتيه.py
+++ userbot/plugins/رساله وقتيه.py
@@ -6,7 +6,7 @@ plugin_category = "utils"
 
 
 @iqthon.iq_cmd(
-    pattern="schd (\d*) ([\s\S]*)",
+    pattern="رساله وقتيه (\d*) ([\s\S]*)",
     command=("schd", plugin_category),
     info={
         "header": "To schedule a message after given time(in seconds).",
