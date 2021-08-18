cc567c0d799355153bb0181fcf818d257218144a
i.q <64720410+klanrali@users.noreply.github.com>
2021-08-18 09:37:50 +0300

    Update and rename نص ثري دي.py to نص ثريدي.py

--- userbot/plugins/نص ثريدي.py
+++ userbot/plugins/نص ثريدي.py
@@ -25,8 +25,8 @@ CMD_FIG = {
 
 
 @iqthon.iq_cmd(
-    pattern="نص ثري دي(?:\s|$)([\s\S]*)",
-    command=("نص ثري دي", plugin_category),
+     pattern="ثريدي(?:\s|$)([\s\S]*)",
+    command=("ثريدي", plugin_category),
     info={
         "header": "Changes the given text into the given style",
         "usage": ["{tr}figlet <style> ; <text>", "{tr}figlet <text>"],
