fa607131f0bbf1d5bbebc584f225823b8f057879
Telethon-Arabs <83516836+telethon-Arab@users.noreply.github.com>
2021-10-24 01:35:12 +0300

    Update الحساب.py

--- userbot/plugins/الحساب.py
+++ userbot/plugins/الحساب.py
@@ -543,15 +543,14 @@ async def fetch_info(replied_user, event):
     last_name = last_name.replace("\u2060", "") if last_name else (" ")
     username = "@{}".format(username) if username else ("`هذا الشخص لايوجد لديه معرف`")
     user_bio = "`هذا الشخص لايوجد لديه نــبــذة`" if not user_bio else user_bio
-    caption = "<b>• ⚜️ | مــعــلــومــات الــمــســتــخــدم :</b>\n"
-    caption += f"<b>• ⚜️ | الاســم  :  </b> `{first_name} {last_name}`\n"
+    caption = "<b>𓍹ⵧⵧⵧⵧⵧⵧⵧⵧᵗᵉˡᵉᵗʰᵒᶰ ᵃʳᵃᵇˢ⁦⁦ⵧⵧⵧⵧⵧⵧⵧⵧ𓍻</b>\n"
+    caption += f"<b>• ⚜️ | الاســم  :  </b> {first_name} {last_name}\n"
     caption += f"<b>• ⚜️ | الــمــ؏ــࢪفہ  : </b> {username}\n"
     caption += f"<b>• ⚜️ | الايــديہ  :  </b> <code>{user_id}</code>\n"
-    caption += f"<b>• ⚜️ | ؏ــدد صــوࢪ  : </b> `{replied_user_profile_photos_count}`\n"
-    caption += f"<b>• ⚜️ | الــنــبــذة  : </b>  `<code>{user_bio}</code>`\n"
-    caption += f"<b>• ⚜️ | الــمــجــمــو؏ــاتہ الـمـشـتـࢪكـة  : </b> `{common_chat}`\n"
-    caption += f"<b>• ⚜️ | رابــط مــبـاشـࢪ لــہ الـحـسـابہ  :  </b> \n"
-    caption += f'• ⚜️ | <a href="tg://user?id={user_id}">{first_name}</a> \n'
+    caption += f"<b>• ⚜️ | ؏ــدد صــوࢪ  : </b> {replied_user_profile_photos_count}\n"
+    caption += f"<b>• ⚜️ | الــنــبــذة  : </b>  <code>{user_bio}</code>\n"
+    caption += f"<b>• ⚜️ | الـحـسـاب  :  </b> "
+    caption += f' <a href="tg://user?id={user_id}">{first_name}</a> \n'
     return photo, caption
 
 async def autoname_loop():
