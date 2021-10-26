ceff7b799881e882cfbeb7d586e7420ff00af764
Telethon-Arabs <83516836+telethon-Arab@users.noreply.github.com>
2021-10-26 03:36:38 +0300

    Update السورس_X.py

--- userbot/plugins/السورس_X.py
+++ userbot/plugins/السورس_X.py
@@ -9,8 +9,10 @@ import asyncio
 import math
 import heroku3
 import urllib3
+import speedtest
 from asyncio.exceptions import CancelledError
 from time import sleep
+from time import time
 from platform import python_version
 from github import Github
 from pySmartDL import SmartDL
@@ -66,6 +68,15 @@ oldvars = {
     "PM_TEXT": "pmpermit_txt",
     "PM_BLOCK": "pmblock",
 }
+
+def convert_from_bytes(size):
+    power = 2 ** 10
+    n = 0
+    units = {0: "", 1: "Kbps", 2: "Mbps", 3: "Gbps", 4: "Tbps"}
+    while size > power:
+        size /= power
+        n += 1
+    return f"{round(size, 2)} {units[n]}"
 @iqthon.on(admin_cmd(pattern="(السورس|سورس)(?: |$)(.*)"))    
 async def amireallyalive(event):
     reply_to_id = await reply_id(event)
@@ -445,3 +456,81 @@ def prettyjson(obj, indent=2, maxlinelength=80):
         indent=indent,
     )
     return indentitems(items, indent, level=0)
+
+@iqthon.on(admin_cmd(pattern="سرعه الانترنيت(?:\s|$)([\s\S]*)"))    
+async def _(event):
+    input_str = event.pattern_match.group(1)
+    as_text = False
+    as_document = False
+    if input_str == "image":
+        as_document = False
+    elif input_str == "file":
+        as_document = True
+    elif input_str == "text":
+        as_text = True
+    catevent = await edit_or_reply(event, "**⌔︙ جـاري حسـاب سرعـه الانـترنيـت لـديك  🔁**")
+    start = time()
+    s = speedtest.Speedtest()
+    s.get_best_server()
+    s.download()
+    s.upload()
+    end = time()
+    ms = round(end - start, 2)
+    response = s.results.dict()
+    download_speed = response.get("download")
+    upload_speed = response.get("upload")
+    ping_time = response.get("ping")
+    client_infos = response.get("client")
+    i_s_p = client_infos.get("isp")
+    i_s_p_rating = client_infos.get("isprating")
+    reply_msg_id = await reply_id(event)
+    try:
+        response = s.results.share()
+        speedtest_image = response
+        if as_text:
+            await catevent.edit(
+                """**⌔︙ حسـاب سرعـه الانـترنيـت لـديك  📶 : {} ثانية**
+
+**⌔︙ التنزيل 📶 :** `{} (or) {} ميغا بايت`
+**⌔︙ الرفع 📶 :** `{} (or) {} ميغا بايت`
+**⌔︙ البنك :** {}` بالثانية`
+**⌔︙ مزود خدمة الإنترنت 📢 :** `{}`
+**⌔︙ تقيم الانترنيت :** `{}`""".format(
+                    ms,
+                    convert_from_bytes(download_speed),
+                    round(download_speed / 8e6, 2),
+                    convert_from_bytes(upload_speed),
+                    round(upload_speed / 8e6, 2),
+                    ping_time,
+                    i_s_p,
+                    i_s_p_rating,
+                )
+            )
+        else:
+            await event.client.send_file(
+                event.chat_id,
+                speedtest_image,
+                caption="**قياس السرعه اكتمل في غضون  `{}`  ثواني **".format(ms),
+                force_document=as_document,
+                reply_to=reply_msg_id,
+                allow_cache=False,
+            )
+            await event.delete()
+    except Exception as exc:
+        await catevent.edit(
+            """**⌔︙ حسـاب سرعـه الانـترنيـت لـديك  📶 : {} ثانية**
+**⌔︙ التنزيل 📶:** `{} (or) {} ميغا بايت`
+**⌔︙ الرفع 📶:** `{} (or) {} ميغا بايت`
+**⌔︙ البنك :** {}` بالثانية`
+
+**⌔︙مع الأخطاء التالية :**
+{}""".format(
+                ms,
+                convert_from_bytes(download_speed),
+                round(download_speed / 8e6, 2),
+                convert_from_bytes(upload_speed),
+                round(upload_speed / 8e6, 2),
+                ping_time,
+                str(exc),
+            )
+        )
