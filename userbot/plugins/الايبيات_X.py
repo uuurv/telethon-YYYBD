541f3da28f4cde09a4b19912f59f78206cc84ddc
Telethon-Arabs <83516836+telethon-Arab@users.noreply.github.com>
2021-10-26 03:00:41 +0300

    Update الايبيات_X.py

--- userbot/plugins/الايبيات_X.py
+++ userbot/plugins/الايبيات_X.py
@@ -68,7 +68,7 @@ telegraph = Telegraph()
 r = telegraph.create_account(short_name=Config.TELEGRAPH_SHORT_NAME)
 auth_url = r["auth_url"]
 
-FONT_FILE_TO_USE = "/userbot/helpers/styles/impact.ttf"
+FONT_FILE_TO_USE = "userbot/helpers/styles/impact.ttf"
 
 
 async def get_tz(con):
