68727eaa1fe7a9da060bb33bfc926137caa9d8fc
Telethon-Arabs <83516836+telethon-Arab@users.noreply.github.com>
2021-10-27 14:09:08 +0300

    Update الاوامر.py

--- userbot/plugins/الاوامر.py
+++ userbot/plugins/الاوامر.py
@@ -4,7 +4,7 @@ from ..core.managers import edit_or_reply
 
 @iqthon.on(admin_cmd(pattern="الاوامر(?: |$)(.*)"))    
 async def iq(event):
-    await edit_or_reply(event, "عذرا الامر قيد الصيانه حاليا لأضافه الاوامر الجديده حاليا الأوامر الشغاله هنا فقط \n https://t.me/M4_STORY")
+    await edit_or_reply(event, "https://t.me/M4_STORY/3439")
 @iqthon.on(admin_cmd(pattern="اعاده تشغيل(?: |$)(.*)"))    
 async def iq(event):
     await edit_or_reply(event, " ⌔︙ عـذرًا، الأمر لقد تغير قم بأرسال فقط  ⬅️ `.تحديث` " "⌔︙ عـذرًا، الأمر لقد تغير قم بأرسال فقط  ⬅️ `.تحديث` ")
