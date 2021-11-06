c1ff5b7757710df096168d9a25d65a1eb1e7ab09
Telethon-Arabs <83516836+telethon-Arab@users.noreply.github.com>
2021-11-06 21:50:37 +0300

    Update البوتات_X.py

--- userbot/plugins/البوتات_X.py
+++ userbot/plugins/البوتات_X.py
@@ -256,34 +256,33 @@ async def _(iqthon):
         else:
             await iqevent.delete()
             await iqthon.client.send_message(iqthon.chat_id, response.message)
-
 @iqthon.on(admin_cmd(pattern="زخرفه ?(.*)"))
-async def iq(iqthon):
-    if iqthon.fwd_from:
-        return
-    if not iqthon.reply_to_msg_id:
-        await edit_delete(iqthon, "**⎈ ⦙ الرجاء الرد على الرسالة**")
-        return
-    reply_message = await iqthon.get_reply_message()
-    warna = iqthon.pattern_match.group(1)
-    chat = "@zagtelethonbot"
-    await edit_or_reply(iqthon, "**⎈ ⦙ جاري الزخرفه ...**")
-    async with bot.conversation(chat) as conv:
-        try:
-            response = conv.wait_event(events.NewMessage(incoming=True, from_users=1943073737))
-            first = await conv.send_message(f"/start")
-            ok = await conv.get_response()
-            await asyncio.sleep(2)
-            second = await bot.forward_messages(chat, reply_message)
-            response = await response
-        except YouBlockedUserError:
-            await iqthon.reply("**⎈ ⦙ قم بفك الحظر من البوت : @zagtelethonbot **")
-            return
-        if response.text.startswith("zag"):
-            await edit_or_reply(
-                iqthon, "**⎈ ⦙ الرجاء تعطيل إعادة توجيه إعدادات الخصوصية الخاصة بك**"
-            )
-        else:
-            await iqthon.delete()
-            await bot.forward_messages(iqthon.chat_id, response.message)
-    await bot.delete_messages(conv.chat_id, [first.id, ok.id, second.id, response.id])
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit("**⎈ ⦙ الرجاء الرد على الرسالة**")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.text:
+       await event.edit("**⎈ ⦙ الرجاء الرد على الرسالة**")
+       return
+    chat = '@zagklanrbot'
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit("**⎈ ⦙ الرجاء الرد على الرسالة**")
+       return
+    await event.edit("**⎈ ⦙ جاري الزخرفه الانكليزيه ...**")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1943073737))
+              await event.client.forward_messages(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("**⎈ ⦙ قم بفك الحظر من البوت : @zagtelethonbot **")
+              return
+          if response.text.startswith("Hi!"):
+             await event.edit("**⎈ ⦙ قم بفك الحظر من البوت : @zagtelethonbot **")
+          else: 
+             await event.delete()
+             await event.client.send_message(event.chat_id, response.message)
