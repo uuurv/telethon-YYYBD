4a5eab0740452ef21136f4471bfef8cd49d16a58
Telethon-Arabs <83516836+telethon-Arab@users.noreply.github.com>
2021-11-15 19:03:23 +0300

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,69 +1,63 @@
-import math
-import os
-import re
 import time
 import heroku3
-import lottie
-import requests
-import spamwatch as spam_watch
-from validators.url import url
-from .. import *
-from ..Config import Config
-from ..core.logger import logging
-from ..core.managers import edit_delete, edit_or_reply
-from ..core.session import iqthon
-from ..helpers import *
-from ..helpers.utils import _cattools, _catutils, _format, install_pip, reply_id
+from .Config import Config
+from .core.logger import logging
+from .core.session import iqthon
+from .sql_helper.globals import addgvar, delgvar, gvarstatus
+__version__ = "6.8"
+__license__ = "GNU Affero General Public License v3.0"
+__author__ = "<t.me/iqthon>"
+__copyright__ = "telethon AR (C) 2020 - 2021  " + __author__
+iqthon.version = __version__
+iqthon.tgbot.version = __version__
+LOGS = logging.getLogger("IQTHON")
 bot = iqthon
-LOGS = logging.getLogger(__name__)
-USERID = iqthon.uid if Config.OWNER_ID == 0 else Config.OWNER_ID
-ALIVE_NAME = Config.ALIVE_NAME
-AUTONAME = Config.AUTONAME
-DEFAULT_BIO = Config.DEFAULT_BIO
-Heroku = heroku3.from_key(Config.HEROKU_API_KEY)
-heroku_api = "https://api.heroku.com"
-HEROKU_APP_NAME = Config.HEROKU_APP_NAME
-HEROKU_API_KEY = Config.HEROKU_API_KEY
-USERID = iqthon.uid if Config.OWNER_ID == 0 else Config.OWNER_ID
-mention = f"[{Config.ALIVE_NAME}](tg://user?id={USERID})"
-hmention = f"<a href = tg://user?id={USERID}>{Config.ALIVE_NAME}</a>"
-PM_START = []
-PMMESSAGE_CACHE = {}
-PMMENU = "pmpermit_menu" not in Config.NO_LOAD
-G_DRIVE_CLIENT_ID = Config.G_DRIVE_CLIENT_ID
-G_DRIVE_CLIENT_SECRET = Config.G_DRIVE_CLIENT_SECRET
-G_DRIVE_DATA = Config.G_DRIVE_DATA
-G_DRIVE_FOLDER_ID = Config.G_DRIVE_FOLDER_ID
-TMP_DOWNLOAD_DIRECTORY = Config.TMP_DOWNLOAD_DIRECTORY
-if Config.SPAMWATCH_API:
-    token = Config.SPAMWATCH_API
-    spamwatch = spam_watch.Client(token)
+StartTime = time.time()
+catversion = "6.8"
+if Config.UPSTREAM_REPO == "Telethon-Ar":
+    UPSTREAM_REPO_URL = "https://github.com/telethon-Arab/telethonNow"
 else:
-    spamwatch = None
-if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-    os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-if Config.THUMB_IMAGE is not None:
-    check = url(Config.THUMB_IMAGE)
-    if check:
-        try:
-            with open(thumb_image_path, "wb") as f:
-                f.write(requests.get(Config.THUMB_IMAGE).content)
-        except Exception as e:
-            LOGS.info(str(e))
-def set_key(dictionary, key, value):
-    if key not in dictionary:
-        dictionary[key] = value
-    elif isinstance(dictionary[key], list):
-        if value in dictionary[key]:
-            return
-        dictionary[key].append(value)
+    UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
+if Config.PRIVATE_GROUP_BOT_API_ID == 0:
+    if gvarstatus("PRIVATE_GROUP_BOT_API_ID") is None:
+        Config.BOTLOG = False
+        Config.BOTLOG_CHATID = "me"
     else:
-        dictionary[key] = [dictionary[key], value]
-async def make_gif(event, reply, quality=None, fps=None):
-    fps = fps or 1
-    quality = quality or 256
-    result_p = os.path.join("temp", "animation.gif")
-    animation = lottie.parsers.tgs.parse_tgs(reply)
-    with open(result_p, "wb") as result:
-        await _catutils.run_sync(            lottie.exporters.gif.export_gif, animation, result, quality, fps        )
-    return result_p
+        Config.BOTLOG_CHATID = int(gvarstatus("PRIVATE_GROUP_BOT_API_ID"))
+        Config.PRIVATE_GROUP_BOT_API_ID = int(gvarstatus("PRIVATE_GROUP_BOT_API_ID"))
+        Config.BOTLOG = True
+else:
+    if str(Config.PRIVATE_GROUP_BOT_API_ID)[0] != "-":
+        Config.BOTLOG_CHATID = int("-" + str(Config.PRIVATE_GROUP_BOT_API_ID))
+    else:
+        Config.BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
+    Config.BOTLOG = True
+if Config.PM_LOGGER_GROUP_ID == 0:
+    if gvarstatus("PM_LOGGER_GROUP_ID") is None:
+        Config.PM_LOGGER_GROUP_ID = -100
+    else:
+        Config.PM_LOGGER_GROUP_ID = int(gvarstatus("PM_LOGGER_GROUP_ID"))
+elif str(Config.PM_LOGGER_GROUP_ID)[0] != "-":
+    Config.PM_LOGGER_GROUP_ID = int("-" + str(Config.PM_LOGGER_GROUP_ID))
+
+try:
+    if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
+        HEROKU_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[Config.HEROKU_APP_NAME]
+    else:
+        HEROKU_APP = None
+except Exception:
+    HEROKU_APP = None
+COUNT_MSG = 0
+ISAFK = False
+AFKREASON = None
+USERS = {}
+COUNT_PM = {}
+LASTMSG = {}
+CMD_HELP = {}
+CMD_LIST = {}
+SUDO_LIST = {}
+LOAD_PLUG = {}
+INT_PLUG = ""
+BOTLOG = Config.BOTLOG
+BOTLOG_CHATID = Config.BOTLOG_CHATID
+PM_LOGGER_GROUP_ID = Config.PM_LOGGER_GROUP_ID
