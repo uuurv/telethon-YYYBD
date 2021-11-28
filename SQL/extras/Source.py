d5fd30cd6d9a50aca99ce8d44199cf5d89cca405
Telethon-Arabs <83516836+telethon-Arab@users.noreply.github.com>
2021-11-28 19:30:57 +0300

    @telethon-Arab
    @telethon-Arab

--- SQL/extras/Source.py
+++ SQL/extras/Source.py
@@ -10,4 +10,4 @@ TG_BOT = Config.TG_BOT_USERNAME
 TM = time.strftime("%I:%M")
 ALIVE_TEXT = "𝗐𝖾𝗅𝖼𝗈𝗆𝖾 𝗍𝖾𝗅𝖾𝗍𝗁𝗈𝗇 𝖺𝗅 𝖺𝗋𝖺𝖻 𓃠"
 EMOJI = "⎈ ⦙"
-Sour = f"**{ALIVE_TEXT}**\n𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n**{EMOJI}  النسخـة :  ِ6.9 ** \n**{EMOJI} حسـابك  :   {mention} **\n**{EMOJI} الـوقت  : {TM} **\n**{EMOJI} البوت :** {TG_BOT}\n**{EMOJI} السـورس :** @IQTHON \n𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+Sour = f"**{ALIVE_TEXT}**\n𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n**{EMOJI}  النسخـة :  ِ7.0 ** \n**{EMOJI} حسـابك  :   {mention} **\n**{EMOJI} الـوقت  : {TM} **\n**{EMOJI} البوت :** {TG_BOT}\n**{EMOJI} السـورس :** @IQTHON \n𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
