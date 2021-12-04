457eb69ac651b58cbf6b8a387d0575446eb065ee
uuurv <95353553+uuurv@users.noreply.github.com>
2021-12-04 03:44:34 +0300

    Update Source.py

--- SQL/extras/Source.py
+++ SQL/extras/Source.py
@@ -5,9 +5,9 @@ from userbot import StartTime, iqthon
 from userbot.Config import Config
 from userbot.plugins import mention
 help1 = ("**⎈ ⦙ كيفيه التنصيب :**")
-help2 = ("**⎈ ⦙ قـائمـه الاوامـر :**\n**⎈ ⦙ قنـاه السـورس :** @IQTHON\n**⎈ ⦙ شـرح اوامـر السـورس : @L3LL3**\n**⎈ ⦙ شـرح فـارات السـورس : @TEAMTELETHON** ")
+help2 = ("**⎈ ⦙ قـائمـه الاوامـر :**\n**⎈ ⦙ قنـاه السـورس :** @IQTHON\n**⎈ ⦙ شـرح اوامـر السـورس : @I_E_A**\n**⎈ ⦙ شـرح فـارات السـورس : @I_E_A** ")
 TG_BOT = Config.TG_BOT_USERNAME
 TM = time.strftime("%I:%M")
-ALIVE_TEXT = "𝗐𝖾𝗅𝖼𝗈𝗆𝖾 𝗍𝖾𝗅𝖾𝗍𝗁𝗈𝗇 𝖺𝗅 𝖺𝗋𝖺𝖻 𓃠"
+ALIVE_TEXT = "𝗐𝖾𝗅𝖼𝗈𝗆𝖾 maison 𓃠"
 EMOJI = "⎈ ⦙"
-Sour = f"**{ALIVE_TEXT}**\n𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n**{EMOJI}  النسخـة :  ِ7.1 ** \n**{EMOJI} حسـابك  :   {mention} **\n**{EMOJI} الـوقت  : {TM} **\n**{EMOJI} البوت :** {TG_BOT}\n**{EMOJI} السـورس :** @IQTHON \n𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+Sour = f"**{ALIVE_TEXT}**\n𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n**{EMOJI}  النسخـة :  ِ7.1 ** \n**{EMOJI} حسـابك  :   {mention} **\n**{EMOJI} الـوقت  : {TM} **\n**{EMOJI} البوت :** {TG_BOT}\n**{EMOJI} السـورس :** @I_E_A \n𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
